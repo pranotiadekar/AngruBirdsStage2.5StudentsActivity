d97c1e234b0f8ac19c253a064773a3520df6e9eb
pranotiadekar <pranoti.adekar39@gmail.com>
2021-01-31 23:35:51 +0530

    Slingshot

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -36,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230,180,80, PI/2);
+    Slingshot= new Slingshot(bird.body,log6.body);
 }
 
 function draw(){
@@ -58,7 +61,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    Slingshot.display(); 
 }
